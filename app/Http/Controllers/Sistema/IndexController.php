116abafab3d693666f03b55eb896a644acb56948
Bruno Roberto Gomes <bruno@brgomes.com>
2019-10-15 14:48:15 -0300

    Ajustando rotas

---
+++ app/Http/Controllers/Sistema/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Sistema;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+    	echo 'Index do sistema';
+    }
+}
