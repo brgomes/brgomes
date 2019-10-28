93eeb58e30bdee3a5c5b61033b67983f38abaf81
Bruno Roberto Gomes <bruno@brgomes.com>
2019-10-28 17:10:27 -0300

    Criando API de autenticação

--- app/Models/Sistema/Usuario.php
+++ app/Models/Sistema/Usuario.php
@@ -5,10 +5,11 @@ namespace App\Models\Sistema;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use OwenIt\Auditing\Contracts\Auditable;
+use Laravel\Passport\HasApiTokens;
 
 class Usuario extends Authenticatable implements Auditable
 {
-    use Notifiable, \OwenIt\Auditing\Auditable;
+    use Notifiable, \OwenIt\Auditing\Auditable, HasApiTokens;
 
     protected $table    = 'sistemaUsuario';
     protected $fillable = ['primeironome', 'sobrenome', 'email', 'senha', 'ultimoacesso', 'timezone'];
