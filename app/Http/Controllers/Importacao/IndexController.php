b81f975df81ba2c69a6618567e6622598312a7f9
Bruno Roberto Gomes <bruno@brgomes.com>
2020-01-09 11:21:15 -0300

    Parando quando não importar Strava

--- app/Http/Controllers/Importacao/IndexController.php
+++ app/Http/Controllers/Importacao/IndexController.php
@@ -48,7 +48,7 @@ class IndexController extends Controller
     		foreach ($strava2 as $strava) {
     			$result = apiRequest('POST', 'strava', ['form_params' => $strava->toArray()]);
 
-                if ($result['statusCode'] != '200') {
+                if ($result->statusCode != '200') {
                     dd($result);
                 }
     		}
