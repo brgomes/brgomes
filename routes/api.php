3baf4000e0724ef9e1bbc531bfa4cea65e111672
Bruno Roberto Gomes <bruno@brgomes.com>
2020-01-09 11:10:14 -0300

    Corrigindo rota

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::group(['middleware' => 'auth:api'], function() {
 	Route::resource('livros', 'API\LivroController');
 
 	Route::get('strava', 'API\StravaController@index')->name('sistema.strava.index');
-	//Route::post('strava', 'API\StravaController@store')->name('sistema.strava.store');
+	Route::post('strava', 'API\StravaController@store')->name('sistema.strava.store');
 
 	Route::post('details', 'API\UsuarioController@details');
 });
