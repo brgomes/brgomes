b2ecdbf669c312790f26df8eedebbd16deea7410
Bruno Roberto Gomes <bruno@brgomes.com>
2020-04-24 11:30:28 -0300

    Atualizando config/app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'brgomes.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://www.brgomes.com'),
 
     /*
     |--------------------------------------------------------------------------
