c35ecdc5d91056d7b470547bae7af33c28f029b5
Bruno Roberto Gomes <bruno@brgomes.com>
2020-05-31 17:36:09 -0300

    Tentando otimizar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -256,9 +256,7 @@
     <!--====== Javascripts & Jquery ======-->
     <script src="{{ asset('js/jquery-2.1.4.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/magnific-popup.min.js') }}"></script>
     <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('js/circle-progress.min.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
 
 
